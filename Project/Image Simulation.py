b419a0c6c76c30a21a125d8872cbba208c0ba950
JustADrop <83846019+JustADrop@users.noreply.github.com>
2021-05-12 15:26:04 -0700

    Update Image Simulation.py

--- Project/Image Simulation.py
+++ Project/Image Simulation.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 """
-Created on Fri May  7 12:47:00 2021
+Created on Wed May 12 12:23:37 2021
 
 @author: Sean
 """
@@ -13,34 +13,23 @@ from prysm.geometry import circle
 from prysm.otf import mtf_from_psf
 from prysm.objects import slantededge, siemensstar
 from prysm.convolution import conv
-from prysm.polynomials import zernike_nm
+from prysm.polynomials import hopkins
 from prysm.detector import bindown
 
 output_res = 100
 wlen = 0.550
 pixel_pitch = 4.5
 f = 50
-fno = 4
+fno = 1.4
 lens_D = f/fno
 lens_R = lens_D/2
 
-# Zernike spherical, coma(+/-), astig(+/-), curv, dist(+/-)
-abers = [(4,0),
-         (3,1),
-         (3,-1),
-         (2,2),
-         (2,-2),
-         (2,0),
-         (1,1),
-         (1,-1)
-    ]
-
-# The above specifications determine a Q of:
+# The above specification determine a Q of:
 Q = (wlen)*(fno)/pixel_pitch
 
 # Need Q_forward >= 2 for forward model, so Q_forward = Q*(oversampling) = 2
 oversampling = ceil(2/Q)
-Q_forward = round(Q*oversampling, 1)
+Q_forward = round(Q*oversampling, 2)
 
 # Intermediate higher res gives psize = pixel_pitch/oversampling
 psize = pixel_pitch/oversampling
@@ -59,41 +48,29 @@ norm_r = r/lens_R
 
 # Construct amplitude function of pupil function
 amp = circle(lens_R, r)
+amp = amp / amp.sum()
 
 # Construct phase mode
-aber = zernike_nm(abers[0][0], abers[0][1], norm_r, theta) # spherical aberration
+aber = hopkins(0, 4, 0, norm_r, theta, 1) # spherical aberration
 
 # Scale phase mode
-# TODO: Look into specific lens MTF's and determine suitable/realistic aberration type and magnitude.
-phase = aber # Will be aber*magnitude
+phase = aber * (wlen/8*1e3)*16
 
-# Construct pupil function from amp and phase functions, propagate to PSF plane, take intensity.
+# Construct pupil function from amp and phase functions, propagate to PSF plane, take square modulus.
 P = Wavefront.from_amp_and_phase(amp, phase, wlen, pup_dx)
 coherent_PSF = P.focus(f, Q=Q_forward)
 PSF = coherent_PSF.intensity
 
 # Plot PSF
 PSF_radius = 1.22*wlen*fno
-PSF.plot2d(xlim=5*PSF_radius, cmap='gray', clim=(0, .1e7))
-
-
-# Hist of PSF
-# hist = plt.hist(np.histogram(PSF.data))
+PSF.plot2d(xlim=5*PSF_radius, cmap='gray', clim=(0, .1))
 
 # Construct MTF from PSF
 MTF = mtf_from_psf(PSF, PSF.dx)
 
-# Construct normalized P for easier plotting
-# norm_P = Wavefront.from_amp_and_phase(amp, phase, wlen, pup_dx)
-# norm_P.data = norm_P.data/np.sum(norm_P.data)
-# norm_coherent_PSF = norm_P.focus(f, Q=Q_forward)
-# norm_PSF = norm_coherent_PSF.intensity
-# norm_PSF_radius = 1.22*(wlen)*fno
-# norm_PSF.plot2d(xlim=norm_PSF_radius*5, cmap='gray', clim=(0,0.3))
-
 fx, _ = MTF.slices().x
-fig, ax = MTF.slices().plot(['x', 'y', 'azavg'], xlim=(0,250), ylim=(0,1))
-ax.plot(fx, ls=':', c='k', alpha=0.75, zorder=1)
+fig, ax = MTF.slices().plot(['x', 'y', 'azavg'], xlim=(0,150), ylim=(0,1))
+ax.axvline(1000/(2*pixel_pitch))
 ax.set(xlabel='Spatial frequency, cy/mm', ylabel='MTF')
 
 # Constuct sample grid for edge/star test patterns
@@ -104,15 +81,15 @@ rho, t = cart_to_polar(x, y)
 # Edge simulation should be done at ~100 output res
 # Star simulation should be done at ~512 output res
 edge = slantededge(x, y)
-# star = siemensstar(rho, t, 40, oradius=110*lens_R)
+star = siemensstar(rho, t, 40, oradius=x.max()*0.8)
 
 # Blur images by convolving with PSF
 convedge = conv(edge, PSF.data)
-# convstar = conv(star, PSF.data)
+convstar = conv(star, PSF.data)
 
 # Resample to output resolution
 edge_image = bindown(convedge, oversampling)
-# star_image = bindown(convstar, oversampling)
+star_image = bindown(convstar, oversampling)
 
 # Plot edge test pattern and blurred image
 fig, axes = plt.subplots(ncols=2, figsize=(10,10))
